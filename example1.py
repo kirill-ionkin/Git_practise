c3c44f931e48bb92d47d1044d863362401089a55
Kirill Ionkin <kirill-ionkin@mail.ru>
2021-04-01 14:42:27 +0300

    Add requirements.txt

---
+++ example1.py
@@ -0,0 +1,48 @@
+import sys
+import os
+
+import numpy as np
+
+
+def f():
+    pass
+
+
+def f2():
+    print("Hel")
+
+
+def f3():
+    pass
+
+
+def f4():
+    pass
+
+
+a = "2"
+b = "2"
+
+c = set()
+d = {}
+
+
+def f():
+
+    pass
+
+
+def f():
+    pass
+
+
+def f2():
+    pass
+
+
+def f4():
+    pass
+
+
+def f6():
+    pass
